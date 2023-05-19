d568ef07d86b4a1fc9e9f948f361b20585c96cf8
MoonjaeChoi <pro301@empal.com>
2023-05-19 17:20:15 +0900

    init

---
+++ src/components/UI/Services.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <section id="services">
+        <div className="container lg:pt-5">
+            <div className="text-center">
+                <h2 className='text-headingColor font-[800] text-[2.4rem] mb-5'>
+                    What do I help
+                </h2>
+                <p className='lg:max-w-[600px] lg:mx-auto text-headingColor font-[500] text-[16px] leading-7'>
+                    Lorem ipsum dolor sit amet consectetur adipjsicinj elit. Quo, minima eaque? Quaerat fuga tenetur eligendi placeat doloremque!
+
+                </p>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Services
